3f6bd681d6a6f78d3ecaca6ea04de457875f1fdf
AlejoFangamer <xxalejoxx71@gmail.com>
2023-06-24 12:20:36 -0500

    Javascript

---
+++ 04-frontend/JS/01 - Tipos de datos/script.js
@@ -0,0 +1,28 @@
+var hola = "Jaja";
+
+//Tipos de Datos
+
+//Primitivos
+
+// 37; //Number
+// true; //Boolean
+// false; //Boolean
+// ("Hola Mundo"); //String
+// undefined;
+// null[
+//   //Objetos
+
+//   (37, "Real Madrid", true)
+// ]; //Array
+
+//Variables
+
+var nombre = "Alejandro Moreno";
+
+var edad = "3700";
+
+//Console log para mostrar algo en consola
+console.log("Hola , Mi nombre es: " + nombre + " y tengo " + edad + " años");
+
+
+alert("Mucho gran hijuepucha")
---
+++ 04-frontend/JS/02 - Funciones/script.js
@@ -0,0 +1,17 @@
+function sumar(x,y) {
+  console.log("La Suma de " + x + " y " + y + " es " + (x + y));
+}
+
+var verduras = 50;
+var levaduras = 15;
+
+var totalCompra = sumar(verduras,levaduras);
+
+console.log(totalCompra)
+
+
+function restar(a,b) {
+  console.log(a-b)
+}
+
+restar(90,36)
