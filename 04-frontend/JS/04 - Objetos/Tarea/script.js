b0efa7b1ded0e5da2742d8d595695a0db363ace5
AlejoFangamer <xxalejoxx71@gmail.com>
2023-06-28 21:09:46 -0500

    Tarea Realizada

---
+++ 04-frontend/JS/04 - Objetos/Tarea/script.js
@@ -0,0 +1,60 @@
+// Definir el objeto de la mascota
+var mascota = {
+  nombre: "nulo",
+  energia: 100,
+  hambre: 50,
+  color: "naranja",
+  estadoEmocional: "Feliz",
+  habilidades: ["Dar la pata", "Dar una vuelta", "Perseguir la pelota"],
+  jugar() {
+    if (this.energia <= 0) {
+      return "¡No tengo suficiente energía para jugar!";
+    } else {
+      this.energia -= 20;
+      this.hambre++;
+      return "Estoy jugando" + "|| Nivel de energía: " + this.energia;
+    }
+  },
+  comer() {
+    if (this.hambre <= 0) {
+      return "No tengo hambre en este momento";
+    } else {
+      this.hambre -= 20;
+      this.energia += 10;
+      return "Estoy comiendo" + "|| Nivel de hambre: " + this.hambre;
+    }
+  },
+  descansar() {
+    if (this.energia >= 100) {
+      return "Tengo suficiente energia, no quiero descansar";
+    } else {
+      this.energia += 10;
+      return "Estoy descansando" + "|| Nivel de energía: " + this.energia;
+    }
+  },
+  estado() {
+    return (
+      "Nivel de energía: " +
+      this.energia +
+      "\nNivel de hambre: " +
+      this.hambre +
+      "\nColor: " +
+      this.color +
+      "\nEstado Emocional: " +
+      this.estadoEmocional +
+      "\nHabilidades: " +
+      this.habilidades
+    );
+  },
+  nombreInicial() {
+    var mascota_nomb = prompt("Cual es el nombre de tu mascota ?");
+    this.nombre = mascota_nomb;
+    console.log("Mi mascota se llama " + mascota_nomb);
+    document.write(this.nombre);
+  },
+};
+
+// Probar el simulador
+console.log("Bienvenido al simulador de mascota");
+mascota.nombreInicial();
+console.log(mascota.estado());
