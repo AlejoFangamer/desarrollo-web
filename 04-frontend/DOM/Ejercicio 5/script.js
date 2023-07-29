034f81177f80f268fffc33079a815de78f07f0f9
AlejoFangamer <xxalejoxx71@gmail.com>
2023-07-29 12:38:15 -0500

    Mas avance en actividad DOM y Explicacion para menu

--- 04-frontend/DOM/Ejercicio 5/script.js
+++ 04-frontend/DOM/Ejercicio 5/script.js
@@ -1,4 +1,4 @@
-let imc, estado , color;
+let imc, estado, color, peso, altura;
 
 const espacio = document.querySelector("#resultados");
 
@@ -7,23 +7,21 @@ const alturaInput = document.querySelector("#int_altura");
 
 const botonInput = document.querySelector("#btn");
 
-const resultado = document.createElement("p");
-const resultado_estado = document.createElement("h2");
-espacio.append(resultado, resultado_estado);
+const resultadoIMC = document.querySelector("#IMC_num");
+const resultadoEstado = document.querySelector("#IMC_estado");
+
+const resultado_accion = document.querySelector("#resultados");
 
 function rango(rango1, rango2) {
   return imc >= rango1 && imc <= rango2;
 }
 
 botonInput.addEventListener("click", () => {
-  const resultado_accion = document.getElementById("resultados");
-  let peso = pesoInput.value;
-  let altura = alturaInput.value;
-  imc = peso / altura ** 2;
+  // Logica del calculo del IMC
+  peso = pesoInput.value;
+  altura = alturaInput.value;
 
-  if (peso === "" || altura === "") {
-    return false;
-  }
+  imc = peso / altura ** 2;
 
   if (imc < 18.5) {
     estado = "Por debajo";
@@ -35,22 +33,47 @@ botonInput.addEventListener("click", () => {
     estado = "Sobrepeso";
     color = "B0E951";
   } else if (rango(30, 34.9)) {
-    estado = "Obesidad 1";
+    estado = "Obesidad #1";
     color = "E9DC51";
   } else if (rango(35, 39.9)) {
-    estado = "Obesidad 2";
+    estado = "Obesidad #2";
     color = "E99E51";
   } else if (imc >= 40) {
-    estado = "Obesidad 3";
+    estado = "Obesidad #3";
     color = "DC4545";
   } else {
     estado = "tas bien supongo ";
   }
 
-  resultado.textContent = `tu IMC es de : ${imc.toFixed(2)}`;
-  resultado_estado.textContent = `${estado}`;
-  resultado_estado.style.color = `#${color}`;
-  resultado_accion.classList.toggle("oculto",resultado.textContent.length < 0);
+  //Imprimir los resultados
+  resultadoIMC.textContent = `tu IMC es de : ${imc.toFixed(2)}`;
+  resultadoEstado.textContent = `${estado}`;
+  resultadoEstado.style.color = `#${color}`;
+
+  //Aparecer y desaparecer los resultados en caso de haber o no haber algo
+  resultado_accion.style.display =
+    resultadoIMC.textContent.length < 0 ? "none" : "flex";
+
+  //Resetear valores y Desactivar el boton
   pesoInput.value = "";
   alturaInput.value = "";
+  checkInputs();
 });
+
+// Funcion de checar si algo en los inputs (Trim ayuda a eliminar los caracteres sin contenido (espacio , tabulacion , puntuacion))
+function checkInputs() {
+  if (pesoInput.value.trim() !== "" && alturaInput.value.trim() !== "") {
+    botonInput.removeAttribute("disabled");
+    botonInput.style.opacity = "1";
+  } else {
+    botonInput.setAttribute("disabled", true);
+    botonInput.style.opacity = "0.5";
+  }
+}
+
+// Inicializar la funcion cada que el usuario cambie algo en el input
+pesoInput.addEventListener("input", checkInputs);
+alturaInput.addEventListener("input", checkInputs);
+
+// Inicializar la funcion cada que inicie la pagina
+checkInputs();
