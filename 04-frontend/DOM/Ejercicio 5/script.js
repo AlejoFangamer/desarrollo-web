8358cb9abc0cb6fb045821a9fe0d98603ae40d57
AlejoFangamer <xxalejoxx71@gmail.com>
2023-07-28 22:40:56 -0500

    "Adelanto en la actividad de DOM de IMC"

--- 04-frontend/DOM/Ejercicio 5/script.js
+++ 04-frontend/DOM/Ejercicio 5/script.js
@@ -1,6 +1,6 @@
-let imc, estado;
+let imc, estado , color;
 
-const calculadora = document.querySelector("#calculadora");
+const espacio = document.querySelector("#resultados");
 
 const pesoInput = document.querySelector("#int_peso");
 const alturaInput = document.querySelector("#int_altura");
@@ -8,13 +8,15 @@ const alturaInput = document.querySelector("#int_altura");
 const botonInput = document.querySelector("#btn");
 
 const resultado = document.createElement("p");
-calculadora.appendChild(resultado);
+const resultado_estado = document.createElement("h2");
+espacio.append(resultado, resultado_estado);
 
 function rango(rango1, rango2) {
   return imc >= rango1 && imc <= rango2;
 }
 
 botonInput.addEventListener("click", () => {
+  const resultado_accion = document.getElementById("resultados");
   let peso = pesoInput.value;
   let altura = alturaInput.value;
   imc = peso / altura ** 2;
@@ -25,21 +27,30 @@ botonInput.addEventListener("click", () => {
 
   if (imc < 18.5) {
     estado = "Por debajo";
+    color = "51BBE9";
   } else if (rango(18.5, 24.9)) {
     estado = "Peso normal";
+    color = "51E984";
   } else if (rango(25, 29.9)) {
     estado = "Sobrepeso";
+    color = "B0E951";
   } else if (rango(30, 34.9)) {
     estado = "Obesidad 1";
+    color = "E9DC51";
   } else if (rango(35, 39.9)) {
     estado = "Obesidad 2";
+    color = "E99E51";
   } else if (imc >= 40) {
     estado = "Obesidad 3";
+    color = "DC4545";
   } else {
     estado = "tas bien supongo ";
   }
 
-  resultado.textContent = `tu IMC es de : ${imc.toFixed(2)} , ${estado}`;
+  resultado.textContent = `tu IMC es de : ${imc.toFixed(2)}`;
+  resultado_estado.textContent = `${estado}`;
+  resultado_estado.style.color = `#${color}`;
+  resultado_accion.classList.toggle("oculto",resultado.textContent.length < 0);
   pesoInput.value = "";
   alturaInput.value = "";
 });
