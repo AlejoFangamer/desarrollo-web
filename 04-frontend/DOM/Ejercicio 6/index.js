1817730105b49f1c9edf2cbd3e1e13a039d27b2c
AlejoFangamer <xxalejoxx71@gmail.com>
2023-08-05 12:09:27 -0500

    Calculadora Final

--- 04-frontend/DOM/Ejercicio 6/index.js
+++ 04-frontend/DOM/Ejercicio 6/index.js
@@ -1,10 +1,10 @@
-import { menuHamburguesa } from "./index/menu_hamburguesa.js";
+
 import { reloj } from "./index/reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   console.log("Holi el DOM se ha cargado");
-  menuHamburguesa(".menu-btn", ".menu-lateral");
+  menuHamburguesa()
   reloj();
 });
--- 04-frontend/DOM/Ejercicio 6/index/calculadora.js
+++ 04-frontend/DOM/Ejercicio 6/index/calculadora.js
@@ -3,4 +3,39 @@ const d = document;
 const pantalla = d.querySelector(".pantalla");
 const botonNumero = d.querySelectorAll(".btn-calculadora");
 
-console.log(pantalla,botonNumero)
+botonNumero.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    let numero = boton.textContent;
+
+    if (boton.id === "igual") {
+      try {
+        pantalla.textContent = eval(pantalla.textContent);
+      } catch (error) {
+        pantalla.textContent = "Erno!";
+      }
+
+      return;
+    }
+
+    if (boton.id === "c") {
+      pantalla.textContent = "0";
+      return;
+    }
+
+    if (boton.id === "borrar") {      
+      if (pantalla.textContent === "" || pantalla.textContent === "error") {
+        pantalla.textContent = "0";
+      }else{
+          pantalla.textContent = pantalla.textContent.slice(0, -1);
+      }
+
+      return;
+    }
+
+    if (pantalla.textContent === "0" || pantalla.textContent === "error") {
+      pantalla.textContent = numero;
+    } else {
+      pantalla.textContent += numero;
+    }
+  });
+});
