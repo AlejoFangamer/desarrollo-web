a76f5ba406fa1cf40d4a0f21b946b866490b7f9f
AlejoFangamer <xxalejoxx71@gmail.com>
2023-08-11 16:22:50 -0500

    Adelanto

--- 04-frontend/DOM/Ejercicio 7/script.js
+++ 04-frontend/DOM/Ejercicio 7/script.js
@@ -1,6 +1,25 @@
 const d = document;
 
 d.addEventListener("DOMContentLoaded", () => {
+  function ajustarTamañoNumeros() {
+    const cuadros = d.querySelectorAll(".num");
+    cuadros.forEach((cuadro) => {
+      const numero = cuadro.querySelector(".num_id");
+      const cuadroWidth =
+        cuadro.clientWidth -
+        (parseFloat(getComputedStyle(cuadro).paddingLeft) +
+          parseFloat(getComputedStyle(cuadro).paddingRight));
+      const numeroWidth = numero.scrollWidth;
+
+      if (numero.scrollWidth > cuadroWidth) {
+        const nuevaEscala = cuadroWidth / numeroWidth;
+        numero.style.transform = `scaleX(${nuevaEscala})`;
+      } else {
+        numero.style.transform = "scaleX(1)";
+      }
+    });
+  }
+
   function leerFetch() {
     fetch("https://jsonplaceholder.typicode.com/comments")
       .then((data) => data.json())
@@ -12,41 +31,33 @@ d.addEventListener("DOMContentLoaded", () => {
           const nombre = d.createElement("h1");
           const comentario = d.createElement("p");
           const contenedor_numero = d.createElement("div");
+          const contenedor_numero_id = d.createElement("div");
           const numero_id = d.createElement("p");
 
-          numero_id.textContent = a.id;
-          numero_id.className = "num_id";
+          contenedor_numero_id.className = "num_id";
           contenedor_numero.className = "num";
+
+          numero_id.textContent = a.id*2;
           comentario.textContent = a.body;
           nombre.textContent = a.name;
-          contenedor_numero.appendChild(numero_id);
+
+          contenedor_numero_id.append(numero_id);
+          contenedor_numero.append(contenedor_numero_id);
           contenedor.append(contenedor_numero, nombre, comentario);
           lista.append(contenedor);
         }
+        ajustarTamañoNumeros();
+      })
+      .catch((erno) => {
+        const main = d.querySelector("#comentarios");
+        const mensaje = d.createElement("h2");
+        mensaje.textContent = `Lo sentimos, no hemos encontramos la información ${erno}`;
+        main.append(mensaje);
       });
   }
 
-  const generarLista = async function () {
-    await leerFetch();
+  const generarLista = function () {
+    leerFetch();
   };
-
   generarLista();
-
-  const cuadros = document.querySelectorAll(".num");
-
-  const ajustarTamañoNumeros = () => {
-    cuadros.forEach((cuadro) => {
-      const numero = cuadro.querySelector(".num_id");
-      const cuadroWidth = cuadro.clientWidth;
-      const fontSize = parseFloat(window.getComputedStyle(numero).fontSize);
-
-      if (numero.scrollWidth > cuadroWidth) {
-        const nuevoFontSize = fontSize * (cuadroWidth / numero.scrollWidth);
-        numero.style.fontSize = nuevoFontSize + "px";
-      }
-    });
-  };
-
-  ajustarTamañoNumeros();
-  window.addEventListener("resize", ajustarTamañoNumeros);
 });
