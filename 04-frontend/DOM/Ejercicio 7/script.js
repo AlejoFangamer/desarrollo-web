fdcda9220656b275a29861d9db12b46a2ec24be1
AlejoFangamer <xxalejoxx71@gmail.com>
2023-08-11 21:22:08 -0500

    Pagina de respuesta Fetch realizada

--- 04-frontend/DOM/Ejercicio 7/script.js
+++ 04-frontend/DOM/Ejercicio 7/script.js
@@ -1,63 +1,68 @@
 const d = document;
 
 d.addEventListener("DOMContentLoaded", () => {
-  function ajustarTamañoNumeros() {
-    const cuadros = d.querySelectorAll(".num");
-    cuadros.forEach((cuadro) => {
-      const numero = cuadro.querySelector(".num_id");
-      const cuadroWidth =
-        cuadro.clientWidth -
-        (parseFloat(getComputedStyle(cuadro).paddingLeft) +
-          parseFloat(getComputedStyle(cuadro).paddingRight));
-      const numeroWidth = numero.scrollWidth;
-
-      if (numero.scrollWidth > cuadroWidth) {
-        const nuevaEscala = cuadroWidth / numeroWidth;
-        numero.style.transform = `scaleX(${nuevaEscala})`;
-      } else {
-        numero.style.transform = "scaleX(1)";
-      }
-    });
-  }
-
   function leerFetch() {
-    fetch("https://jsonplaceholder.typicode.com/comments")
+    fetch("https://jsonplaceholder.typicode.com/posts")
       .then((data) => data.json())
       .then((resp) => {
         for (const a of resp) {
           const lista = d.querySelector("#lista");
-
           const contenedor = d.createElement("li");
-          const nombre = d.createElement("h1");
-          const comentario = d.createElement("p");
-          const contenedor_numero = d.createElement("div");
-          const contenedor_numero_id = d.createElement("div");
-          const numero_id = d.createElement("p");
 
+          const contenedor_post = d.createElement("div");
+
+          const [titulo, comentario] = [
+            d.createElement("h2"),
+            d.createElement("p"),
+          ];
+
+          contenedor_post.className = "post";
+
+          comentario.textContent = a.body;
+          titulo.textContent = a.title;
+
+          const [
+            imagen_id,
+            contenedor_numero_id,
+            contenedor_numero,
+            numero_id,
+          ] = [
+            d.createElement("img"),
+            d.createElement("div"),
+            d.createElement("div"),
+            d.createElement("p"),
+          ];
+
+          imagen_id.className = "imagen-usuario";
           contenedor_numero_id.className = "num_id";
           contenedor_numero.className = "num";
 
-          numero_id.textContent = a.id*2;
-          comentario.textContent = a.body;
-          nombre.textContent = a.name;
+          numero_id.textContent = a.id;
+          imagen_id.src = "Icono.png";
 
           contenedor_numero_id.append(numero_id);
-          contenedor_numero.append(contenedor_numero_id);
-          contenedor.append(contenedor_numero, nombre, comentario);
+          contenedor_numero.append(imagen_id, contenedor_numero_id);
+
+          contenedor_post.append(titulo, comentario);
+
+          contenedor.append(contenedor_numero, contenedor_post);
           lista.append(contenedor);
         }
-        ajustarTamañoNumeros();
       })
       .catch((erno) => {
-        const main = d.querySelector("#comentarios");
-        const mensaje = d.createElement("h2");
-        mensaje.textContent = `Lo sentimos, no hemos encontramos la información ${erno}`;
-        main.append(mensaje);
+        const main = d.querySelectorAll(".error");
+        const mensaje = d.querySelector("#error-mensaje");
+
+        main.forEach((error) => {
+          error.style.display = "flex";
+        });
+
+        mensaje.textContent = `${erno}`;
       });
   }
 
-  const generarLista = function () {
-    leerFetch();
+  const generarLista = async function () {
+    await leerFetch();
   };
   generarLista();
 });
